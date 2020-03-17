782b074bfc912bb63504ba1c9942c43ffbe1f85d
Bohdan-creator <56387230+Bohdan-creator@users.noreply.github.com>
2020-03-17 20:04:00 +0100

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SchoolRegister.BLL.Entities;
+using SchoolRegister.DAL.EF;
+
+using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using SchoolRegisterSystem.DAL.EF;
+
+namespace SchoolRegister.Web
+{
+    public class Startup
+    {
+        private readonly string _connectionString;
+        public IHostingEnvironment HostingEnvironment { get; set; }
+        public IServiceCollection Services { get; private set; }
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            if (env.IsEnvironment("Development"))
+            {
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            _connectionString = Configuration["ConnectionStrings:MsSqlConnection"];
+            HostingEnvironment = env;
+            Configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region Framework Services
+            // Add framework services.
+            services.AddOptions();
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+           
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+                options.Cookie.HttpOnly = true;
+            });
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(_connectionString);  // SQL SERVER
+
+            });
+            services.AddIdentity<User, Role>()
+                .AddRoles<Role>()
+                .AddRoleManager<RoleManager<Role>>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddSignInManager<SignInManager<User>>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
+            services.AddScoped<IUserClaimsPrincipalFactory<User>, UserClaimsPrincipalFactory<User, Role>>();
+            services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
+            services.Configure<FormOptions>(x =>
+            {
+                x.ValueLengthLimit = int.MaxValue;
+                x.MultipartBodyLengthLimit = int.MaxValue;
+                x.KeyLengthLimit = int.MaxValue;
+            });
+
+            
+
+    
+
+         
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddSignalR();
+            #endregion
+
+            #region Our Services
+            var cs = new ConnectionStringDto() { ConnectionString = _connectionString };
+            services.AddSingleton(cs);
+           
+            services.AddScoped<DbContext, ApplicationDbContext>();
+            services.AddScoped<DbContextOptions<ApplicationDbContext>>();
+            #endregion
+            Services = services;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            var localizationOption = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(localizationOption.Value);
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseRouting();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+        }
+    }
+}
